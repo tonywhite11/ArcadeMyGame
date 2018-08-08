c06f776279392a493bcfafc2a882bb264487e20b
Bruce Bryant <plumfish464@gmail.com>
2018-08-08 08:58:05 -0700

    Reset game to original form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
     this.x = x;
@@ -5,103 +6,66 @@ var Enemy = function(x, y, speed) {
     this.speed = speed; 
     this.sprite = 'images/enemy-bug.png';  // The image/sprite for our enemies
     this.step = 101;
-    this.boundary = this.step * 8;
+    this.boundary = this.step * 5;
     this.resetPos = -this.step;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
 
-    // If enemy is not passed boundary
+// If enemy is not passed boundary
     if(this.x < this.boundary) {
        // Move forward
        // Increment x by speed * dt 
        this.x += this.speed * dt;
     }
     else {
-       // Reset pos to start
+// Reset pos to start
        this.x = this.resetPos;
     }
 };
-
-
-// New Hero object
-
-// Ini allenemies array
-// For each enemy create and push new Enemy object into above array
-
-
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on canvas
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-// Hero class constructor
+// Hero Constructor
 class Hero {
     constructor() {
-        this.sprite = 'images/char-boy.png';
+        this.sprite = 'images/char-boy.png';    // Properties and x, y pos
         this.step = 101;
         this.jump = 83;
-        this.startX = this.step * 2;
+        this.startX = this.step * 3;
         this.startY = (this.jump * 4) + 55;
         this.x = this.startX;
         this.y = this.startY;
         this.victory = false;
        }
-
-    // draw hero sprite on current x and y coord position
+// Sprite image
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
-    
-    // Draw
-   // Constructor
-       // Properties
-          // x pos
-          // y pos
-          // Sprite image
-       // Methods
-       // Update position
-          update() {
+       }
+// Update position
+    update() {
 
-             // Check collision here
-             for(let enemy of allEnemies) {
-
-                // Did player x and y collide with enemy?
-                if (this.y === enemy.y && (enemy.x + enemy.step/2 > this.x && enemy.x < this.x + this.step/2)) {
-                    this.reset();
-                }
-             };
-             // Check if won
-             if(this.y === 55) {
-                this.victory = true;
-             }
-          }
-         // Reset Hero
-          reset() {
-            this.y = this.startY;
-            this.x = this.startX;
-          }
-          // Render
-             // Draw player sprite on current x and y coord position
-          // Handle keyboard input
-                // Update player's x and y property according to input
-         
-        
-    /**
-     * Update hero's x and y property according to input
-     *
-     * @param {string} input - Direction to travel
-     */
-     handleInput(input) {
+// Check collision 
+        for(let enemy of allEnemies) {
+        if (this.y === enemy.y && (enemy.x + enemy.step/2 > this.x && enemy.x < this.x + this.step/2)) {
+              this.reset();
+            }
+        };
+// Check if won
+        if(this.y === 55) {
+              this.victory = true;
+            }
+        }
+// Reset Hero
+    reset() {
+              this.y = this.startY;
+              this.x = this.startX;
+        }
+// HandleInput method
+    handleInput(input) {
         switch(input) {
             case 'left':
                if (this.x > 0) {
@@ -127,21 +91,16 @@ class Hero {
 
      }
 }
+// New Hero object
 const player = new Hero();
-const bug1 = new Enemy(-101, 0, 350);
-const bug2 = new Enemy(-101, 83, 100);
-const bug3 = new Enemy((-101*5), 83, 375);
+// Init allenemies array
+const bug1 = new Enemy(-101, 0, 500);
+const bug2 = new Enemy(-101, 83, 150);
+const bug3 = new Enemy((-101*5), 83, 350);
+const bug4 = new Enemy(-101*5, 166, 270);
+const bug5 = new Enemy(-101*2, 249, 300);
 const allEnemies = [];
-allEnemies.push(bug1,bug2,bug3);
-
-
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
+allEnemies.push(bug1,bug2,bug3,bug4,bug5);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
--- js/engine.js
+++ js/engine.js
@@ -14,10 +14,6 @@
  */
 
 var Engine = (function(global) {
-    /* Predefine the variables we'll be using within this scope,
-     * create the canvas element, grab the 2D context for that canvas
-     * set the canvas elements height/width and add it to the DOM.
-     */
     var doc = global.document,
         win = global.window,
         canvas = doc.createElement('canvas'),
@@ -38,37 +34,18 @@ var Engine = (function(global) {
         win.requestAnimationFrame(main);
     });
 
-    canvas.width = 808;
+    canvas.width = 505;
     canvas.height = 606;
     doc.body.appendChild(canvas);
 
-    /* This function serves as the kickoff point for the game loop itself
-     * and handles properly calling the update and render methods.
-     */
     function main() {
-        /* Get our time delta information which is required if your game
-         * requires smooth animation. Because everyone's computer processes
-         * instructions at different speeds we need a constant value that
-         * would be the same for everyone (regardless of how fast their
-         * computer is) - hurray time!
-         */
         var now = Date.now(),
             dt = (now - lastTime) / 1000.0;
-
-        /* Call our update/render functions, pass along the time delta to
-         * our update function since it may be used for smooth animation.
-         */
         update(dt);
         render();
 
-        /* Set our lastTime variable which is used to determine the time delta
-         * for the next time this function is called.
-         */
         lastTime = now;
 
-        /* Use the browser's requestAnimationFrame function to call this
-         * function again as soon as the browser is able to draw another frame.
-         */
          if (player.victory === true) {
             win.cancelAnimationFrame(id);
             toggleModal();
@@ -137,6 +114,7 @@ var Engine = (function(global) {
             numRows = 6,
             numCols = 9,
             row, col;
+
         
         // Before drawing, clear existing canvas
         ctx.clearRect(0,0,canvas.width,canvas.height)
@@ -160,15 +138,9 @@ var Engine = (function(global) {
 
         renderEntities();
     }
+    
 
-    /* This function is called by the render function and is called on each game
-     * tick. Its purpose is to then call the render functions you have defined
-     * on your enemy and player entities within app.js
-     */
     function renderEntities() {
-        /* Loop through all of the objects within the allEnemies array and call
-         * the render function you have defined.
-         */
          allEnemies.forEach(function(enemy) {
              enemy.render();
          });
@@ -189,10 +161,7 @@ var Engine = (function(global) {
     const modal = document.querySelector('.modal_bg');
     modal.classList.toggle('hide');
     }
-    /* Go ahead and load all of the images we know we're going to need to
-     * draw our game level. Then set init as the callback method, so that when
-     * all of these images are properly loaded our game will start.
-     */
+ 
     Resources.load([
         'images/stone-block.png',
         'images/water-block.png',
@@ -202,9 +171,5 @@ var Engine = (function(global) {
     ]);
     Resources.onReady(init);
 
-    /* Assign the canvas' context object to the global variable (the window
-     * object when run in a browser) so that developers can use it more easily
-     * from within their app.js files.
-     */
     global.ctx = ctx;
 })(this);
